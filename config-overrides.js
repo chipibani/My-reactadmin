db8f737cde5c25cabeb6b2928ad21b2e125fe8f9
吃屁吧你 <12345679@qq.com>
2020-01-12 21:44:17 +0800

    “全部提交

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+ const { override, fixBabelImports, addLessLoader,addDecoratorsLegacy ,addWebpackAlias} = require('customize-cra');
+
+module.exports = override(
+  //按需加载
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+   style: true,
+  }),
+  //自定义主题
+ addLessLoader({
+   javascriptEnabled: true,
+   modifyVars: { '@primary-color': '#1DA57A' },
+ }),
+ //es7装饰器语法
+ addDecoratorsLegacy(),
+ //配置路径别名
+ addWebpackAlias({}),
+ 
+);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDOM from "react-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { Provider } from "react-redux";
+
+import App from "./App";
+
+import store from './components/redux/store';
+
+import './index.less';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
